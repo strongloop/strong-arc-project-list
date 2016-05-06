7ef667c09156d1591d7c43cc4e02db6630fd3b5d
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 16:46:21 -0700

    update copyright notices and license

--- server/boot/explorer.js
+++ server/boot/explorer.js
@@ -1,4 +1,4 @@
-// Copyright IBM Corp. 2015. All Rights Reserved.
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
 // Node module: strong-arc-project-list
 // US Government Users Restricted Rights - Use, duplication or disclosure
 // restricted by GSA ADP Schedule Contract with IBM Corp.
